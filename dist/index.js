154ba27f2023cd3977462827d6e9095922fd899d
PRANESH-KAAYLABS <pranesh.manickam@kaaylabs.com>
2023-07-11 12:26:27 +0530

    dist changes

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,2 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import PraneshText from './component/pranesh';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render( /*#__PURE__*/React.createElement(React.StrictMode, null, /*#__PURE__*/React.createElement(PraneshText, null)));
+export default PraneshText;
