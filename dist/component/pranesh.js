14b8272d79d0c91debb48d972771429c79a71991
PRANESH-KAAYLABS <pranesh.manickam@kaaylabs.com>
2023-07-11 12:21:42 +0530

    dist only

---
+++ dist/component/pranesh.js
@@ -0,0 +1,5 @@
+import React from "react";
+const PraneshText = () => {
+  return /*#__PURE__*/React.createElement("h1", null, "Hi pranesh, this is your package text...");
+};
+export default PraneshText;
