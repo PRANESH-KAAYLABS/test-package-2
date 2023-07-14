b6c2734fb02de51770da1b6a319f72bface6d64d
PRANESH-KAAYLABS <pranesh.manickam@kaaylabs.com>
2023-07-14 12:55:24 +0530

    git

--- dist/component/pranesh.js
+++ dist/component/pranesh.js
@@ -6,6 +6,6 @@ const PraneshText = props => {
   } = props;
   return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, "Hi ", name, ", this is your package text..."), /*#__PURE__*/React.createElement(Button, {
     variant: "contained"
-  }, "Hello "));
+  }, "Hello "), /*#__PURE__*/React.createElement("button", null, "hello gokul"));
 };
 export default PraneshText;
