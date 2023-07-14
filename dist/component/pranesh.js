fbe570a7cb9ac698632fe87421fc704690e0e32c
PRANESH-KAAYLABS <pranesh.manickam@kaaylabs.com>
2023-07-14 12:49:26 +0530

    mui

--- dist/component/pranesh.js
+++ dist/component/pranesh.js
@@ -1,8 +1,11 @@
 import React from "react";
+import { Button } from "@mui/material";
 const PraneshText = props => {
   const {
     name
   } = props;
-  return /*#__PURE__*/React.createElement("h1", null, "Hi ", name, ", this is your package text...");
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, "Hi ", name, ", this is your package text..."), /*#__PURE__*/React.createElement(Button, {
+    variant: "contained"
+  }, "Hello "));
 };
 export default PraneshText;
